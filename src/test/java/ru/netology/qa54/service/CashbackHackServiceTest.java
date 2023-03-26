f1dcebe6b719b78b00168dd84a1e27b4f1b1c74f
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-03-26 13:19:02 +0300

    Test failed

--- src/test/java/ru/netology/qa54/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/qa54/service/CashbackHackServiceTest.java
@@ -1,51 +1,59 @@
 package ru.netology.qa54.service;
 
-import static org.testng.Assert.*;
-
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void testRemainOne() {
 
         int actual = service.remain(400);
         int expected = 600;
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void testRemainTwo() {
 
         int actual = service.remain(0);
         int expected = 1000;
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void testRemainThree() {
 
         int actual = service.remain(999);
         int expected = 1;
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void testRemainFour() {
 
         int actual = service.remain(1);
         int expected = 999;
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
+
+    }
+    @org.junit.Test
+
+    public void testRemainMistakeInService() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        org.junit.Assert.assertEquals(actual, expected);
 
     }
 
